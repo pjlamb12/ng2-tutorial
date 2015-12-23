db6533bcf9e0869023216945acb1b15e8922254c
Preston Lamb <preston.j.lamb@gmail.com>
2015-12-23 09:30:41 -0700

    chapter 2 of the tour of heroes tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -8,19 +8,55 @@ interface Hero {
 @Component({
 	selector: 'my-app',
 	template: `
-	<h1>{{title}}</h1>
-	<h2>{{hero.name}} details!</h2>
-	<div><label>id: </label>{{hero.id}}</div>
-	<div>
-		<label>name: </label>
-		<div><input [(ngModel)]="hero.name" placeholder="name"></div>
-	</div>`
+	<h2>My Heroes</h2>
+	<ul class="heroes">
+		<li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
+			<span class="badge">{{hero.id}}</span> {{hero.name}}
+		</li>
+	</ul>
+	<div *ngIf="selectedHero">
+		<h2>{{selectedHero.name}} details!</h2>
+		<div><label>id: </label>{{selectedHero.id}}</div>
+		<div>
+			<label>name: </label>
+			<div><input [(ngModel)]="selectedHero.name" placeholder="name"></div>
+		</div>
+	</div>
+	`,
+	styles:[`
+		.heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
+		.heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
+		.heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
+		.heroes .badge {
+			font-size: small;
+			color: white;
+			padding: 0.1em 0.7em;
+			background-color: #369;
+			line-height: 1em;
+			position: relative;
+			left: -1px;
+			top: -1px;
+		}
+		.selected { background-color: #EEE; color: #369; }
+	`]
 })
 
 export class AppComponent {
 	public title = 'Tour of Heroes';
-	public hero = Hero = {
-		id: 1,
-		name: 'Windstorm'
-	};
+	public selectedHero: Hero;
+	public heroes = HEROES;
+	onSelect(hero: Hero) { this.selectedHero = hero; }
 }
+
+var HEROES: Hero[] = [
+	{ "id": 11, "name": "Mr. Nice" },
+	{ "id": 12, "name": "Narco" },
+	{ "id": 13, "name": "Bombasto" },
+	{ "id": 14, "name": "Celeritas" },
+	{ "id": 15, "name": "Magneta" },
+	{ "id": 16, "name": "RubberMan" },
+	{ "id": 17, "name": "Dynama" },
+	{ "id": 18, "name": "Dr IQ" },
+	{ "id": 19, "name": "Magma" },
+	{ "id": 20, "name": "Tornado" }
+]
