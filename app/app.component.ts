3c64f04ca0fea5b4530259ba8318b5dfd711ad92
Preston Lamb <pjlamb12@Prestons-MacBook-Pro.local>
2016-02-19 13:46:14 -0700

    updated the angular 2 tutorial

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,53 +1,82 @@
-import {Component} from 'angular2/core';
+import {Component, OnInit} from 'angular2/core';
 import {Hero} from './hero';
 import {HeroDetailComponent} from './hero-detail.component';
 import {HeroService} from './hero.service';
-import {OnInit} from 'angular2/core';
-
 @Component({
-	selector: 'my-app',
-	template: `
-	<h2>My Heroes</h2>
-	<ul class="heroes">
-		<li *ngFor="#hero of heroes" (click)="onSelect(hero)" [class.selected]="hero === selectedHero">
-			<span class="badge">{{hero.id}}</span> {{hero.name}}
-		</li>
-	</ul>
-	<my-hero-detail [hero]="selectedHero"></my-hero-detail>
-	`,
-	styles:[`
-		.heroes {list-style-type: none; margin-left: 1em; padding: 0; width: 10em;}
-		.heroes li { cursor: pointer; position: relative; left: 0; transition: all 0.2s ease; }
-		.heroes li:hover {color: #369; background-color: #EEE; left: .2em;}
-		.heroes .badge {
-			font-size: small;
-			color: white;
-			padding: 0.1em 0.7em;
-			background-color: #369;
-			line-height: 1em;
-			position: relative;
-			left: -1px;
-			top: -1px;
-		}
-		.selected { background-color: #EEE; color: #369; }
-	`],
-	directives: [HeroDetailComponent],
-	providers: [HeroService]
+  selector: 'my-app',
+  template:`
+    <h1>{{title}}</h1>
+    <h2>My Heroes</h2>
+    <ul class="heroes">
+      <li *ngFor="#hero of heroes"
+        [class.selected]="hero === selectedHero"
+        (click)="onSelect(hero)">
+        <span class="badge">{{hero.id}}</span> {{hero.name}}
+      </li>
+    </ul>
+    <my-hero-detail [hero]="selectedHero"></my-hero-detail>
+  `,
+  styles:[`
+    .selected {
+      background-color: #CFD8DC !important;
+      color: white;
+    }
+    .heroes {
+      margin: 0 0 2em 0;
+      list-style-type: none;
+      padding: 0;
+      width: 10em;
+    }
+    .heroes li {
+      cursor: pointer;
+      position: relative;
+      left: 0;
+      background-color: #EEE;
+      margin: .5em;
+      padding: .3em 0em;
+      height: 1.6em;
+      border-radius: 4px;
+    }
+    .heroes li.selected:hover {
+      color: white;
+    }
+    .heroes li:hover {
+      color: #607D8B;
+      background-color: #EEE;
+      left: .1em;
+    }
+    .heroes .text {
+      position: relative;
+      top: -3px;
+    }
+    .heroes .badge {
+      display: inline-block;
+      font-size: small;
+      color: white;
+      padding: 0.8em 0.7em 0em 0.7em;
+      background-color: #607D8B;
+      line-height: 1em;
+      position: relative;
+      left: -1px;
+      top: -4px;
+      height: 1.8em;
+      margin-right: .8em;
+      border-radius: 4px 0px 0px 4px;
+    }
+  `],
+  directives: [HeroDetailComponent],
+  providers: [HeroService]
 })
-
 export class AppComponent implements OnInit {
-	public title = 'Tour of Heroes';
-	public selectedHero: Hero;
-
-	onSelect(hero: Hero) { this.selectedHero = hero; }
-
-	getHeroes() {
-		this._heroService.getHeroes().then(heroes => this.heroes = heroes);
-	}
-
-	constructor(private _heroService: HeroService) {}
-
-	ngOnInit() {
-		this.getHeroes();
-	}
+  title = 'Tour of Heroes';
+  heroes: Hero[];
+  selectedHero: Hero;
+  constructor(private _heroService: HeroService) { }
+  getHeroes() {
+    this._heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+  ngOnInit() {
+    this.getHeroes();
+  }
+  onSelect(hero: Hero) { this.selectedHero = hero; }
 }
